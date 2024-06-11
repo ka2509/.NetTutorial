70ac2d817f9f2153998bc6979790a4ef5f9d3533
2uangAnh <doquanganhzzz2k3@gmail.com>
2024-06-11 17:03:25 +0700

    api for get method

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -2,11 +2,36 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using api.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace api.Controllers
 {
-    public class StockController
+    [Route("api/stock")]
+    [ApiController]
+    public class StockController : ControllerBase
     {
+        private readonly ApplicationDBContext _context;
+        public StockController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll() {
+            // read about deferred execution
+            // So the query is not executed when it is defined or when its methods are called. So at this point, we need an ToList() to actually get the data out.
+            var stocks = _context.Stock.ToList();
+            return Ok(stocks);
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetById([FromRoute] int id) {
+            var stock = _context.Stock.Find(id);
+            if(stock == null) {
+                return NotFound();
+            }
+            return Ok(stock);
+        }
         
     }
 }
--- api/Data/ApplicationDBContext.cs
+++ api/Data/ApplicationDBContext.cs
@@ -14,7 +14,9 @@ namespace api.Data
 {
     public class ApplicationDBContext : DbContext
     {
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
         public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options)
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
             : base(options)
         {
         }
