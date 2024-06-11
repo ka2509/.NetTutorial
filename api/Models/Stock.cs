1e8345d768a9400f3ed74677c557623225125b86
2uangAnh <doquanganhzzz2k3@gmail.com>
2024-06-11 14:15:19 +0700

    first commit

---
+++ api/Models/Stock.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Models
+{
+    public class Stock
+    {
+        public int Id {get; set; }
+        public string Symbol {get; set; } = string.Empty;
+        public string CompanyName {get; set; } = string.Empty;
+        // define the format of the value in this column
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Purchase {get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal LastDiv {get; set; }
+        public string Industry {get; set; } = string.Empty;
+        public long MarketCap {get; set; }
+
+        //Mapping
+        //One To Many
+        public List<Comment> Comments {get; set; } = new List<Comment>;
+    }
+}   
