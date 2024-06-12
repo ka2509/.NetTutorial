7f299a111b37528dfda6b32c79b2e4d4822a2b96
2uangAnh <doquanganhzzz2k3@gmail.com>
2024-06-12 18:00:19 +0700

    modify controller and implement overide method at repository

--- api/Repository/StockRepository.cs
+++ api/Repository/StockRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Data;
+using api.Dtos.StockD;
 using api.Interfaces;
 using api.Models;
 using Microsoft.EntityFrameworkCore;
@@ -16,9 +17,50 @@ namespace api.Repository
             {
                 _context = context;
             }
+
+        public async Task<Stock> CreateAsync(Stock stockModel)
+        {
+            await _context.Stock.AddAsync(stockModel);
+            await _context.SaveChangesAsync();
+            return stockModel;
+        }
+
+        public async Task<Stock?> DeleteAsync(int id)
+        {
+            var stock = await _context.Stock.FirstOrDefaultAsync(s => s.Id == id);
+            
+            if(stock == null) {
+                return null;
+            }
+            _context.Stock.Remove(stock);
+            await _context.SaveChangesAsync();
+            return stock;
+        }
+
         public Task<List<Stock>> GetAllAsync()
         {
             return _context.Stock.ToListAsync();
+        }
+
+        public async Task<Stock?> GetByIdAsync(int id)
+        {
+            return await _context.Stock.FindAsync(id);
+        }   
+
+        public async Task<Stock?> UpdateAsync(int id, UpdateStockRequestDto updateDto)
+        {
+            var stockModel = await _context.Stock.FirstOrDefaultAsync(s => s.Id == id);
+            if(stockModel == null) {
+                return null;
+            }
+            stockModel.Symbol = updateDto.Symbol;
+            stockModel.CompanyName = updateDto.CompanyName;
+            stockModel.Purchase = updateDto.Purchase;
+            stockModel.LastDiv = updateDto.LastDiv;
+            stockModel.Industry = updateDto.Industry;
+            stockModel.MarketCap = updateDto.MarketCap;
+            await _context.SaveChangesAsync();
+            return stockModel;
         }
     }
 }
